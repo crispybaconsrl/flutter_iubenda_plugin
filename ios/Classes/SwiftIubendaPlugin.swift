894f4e4334e4f2a304feb95cbfb55814bb8e56a0
AlessandroMenicali <alessandro.menicali@crispybacon.it>
2023-02-08 10:41:00 +0100

    Manually linked iubenda.xcframework to iOS Plugin

--- ios/Classes/SwiftIubendaPlugin.swift
+++ ios/Classes/SwiftIubendaPlugin.swift
@@ -1,14 +1,19 @@
 import Flutter
 import UIKit
+import iubenda
 
 public class SwiftIubendaPlugin: NSObject, FlutterPlugin {
-  public static func register(with registrar: FlutterPluginRegistrar) {
-    let channel = FlutterMethodChannel(name: "iubenda_plugin", binaryMessenger: registrar.messenger())
-    let instance = SwiftIubendaPlugin()
-    registrar.addMethodCallDelegate(instance, channel: channel)
-  }
-
-  public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-    result("iOS " + UIDevice.current.systemVersion)
-  }
+    
+    
+    public static func register(with registrar: FlutterPluginRegistrar) {
+        let channel = FlutterMethodChannel(name: "iubenda_plugin", binaryMessenger: registrar.messenger())
+        let instance = SwiftIubendaPlugin()
+        registrar.addMethodCallDelegate(instance, channel: channel)
+        registrar.addApplicationDelegate(instance)
+        
+    }
+    
+    public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
+        result("iOS " + UIDevice.current.systemVersion)
+    }
 }
--- ios/iubenda_plugin.podspec
+++ ios/iubenda_plugin.podspec
@@ -15,9 +15,12 @@ A new Flutter project.
   s.source           = { :path => '.' }
   s.source_files = 'Classes/**/*'
   s.dependency 'Flutter'
-  s.platform = :ios, '9.0'
+  s.platform = :ios, '10.0'
 
   # Flutter.framework does not contain a i386 slice.
   s.pod_target_xcconfig = { 'DEFINES_MODULE' => 'YES', 'EXCLUDED_ARCHS[sdk=iphonesimulator*]' => 'i386' }
   s.swift_version = '5.0'
+  s.preserve_paths = 'iubenda.xcframework/**/*'
+  s.xcconfig = { 'OTHER_LDFLAGS' => '-framework iubenda' }
+  s.vendored_frameworks = 'iubenda.xcframework'
 end
