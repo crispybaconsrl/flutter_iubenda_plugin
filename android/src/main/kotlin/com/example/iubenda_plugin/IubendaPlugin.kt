362066348f7cc79be3c9904424afb73e3730de26
andreabrugaletta <andreabrugaletta@yahoo.it>
2026-05-06 16:54:48 +0200

    fix: prevent success onActivityResult to be called twice in the same session

--- android/src/main/kotlin/com/example/iubenda_plugin/IubendaPlugin.kt
+++ android/src/main/kotlin/com/example/iubenda_plugin/IubendaPlugin.kt
@@ -23,6 +23,7 @@ class IubendaPlugin : FlutterPlugin, MethodCallHandler, ActivityAware,
     /// when the Flutter Engine is detached from the Activity
     private lateinit var channel: MethodChannel
     private lateinit var activity: Activity
+    private var activityBinding: ActivityPluginBinding? = null
     private  var consentResult: Result? = null
     private val CHECK_CONSENT_FROM_IUBENDA = 367
 
@@ -60,13 +61,15 @@ class IubendaPlugin : FlutterPlugin, MethodCallHandler, ActivityAware,
     }
 
     override fun onAttachedToActivity(binding: ActivityPluginBinding) {
+        activityBinding = binding
         binding.addActivityResultListener(this)
         channel.setMethodCallHandler(this)
         activity = binding.activity
     }
 
     override fun onDetachedFromActivityForConfigChanges() {
-
+        activityBinding?.removeActivityResultListener(this)
+        activityBinding = null
     }
 
     override fun onReattachedToActivityForConfigChanges(binding: ActivityPluginBinding) {
@@ -74,7 +77,8 @@ class IubendaPlugin : FlutterPlugin, MethodCallHandler, ActivityAware,
     }
 
     override fun onDetachedFromActivity() {
-
+        activityBinding?.removeActivityResultListener(this)
+        activityBinding = null
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?): Boolean {
@@ -89,11 +93,11 @@ class IubendaPlugin : FlutterPlugin, MethodCallHandler, ActivityAware,
                     json.put("google_ads",isGoogleAdsPersonalised)
 
                     consentResult!!.success(json.toString())
+                    consentResult = null
                     return consentValue;
                 }
             }
         }
         return false
     }
-
 }
